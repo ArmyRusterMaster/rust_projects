95dac082b165f66dbcd5edbaf63c2ec69caf6597
Your Name <you@example.com>
2026-04-18 10:04:59 +0500

    locolern

---
+++ lernloco/src/initializers/view_engine.rs
@@ -0,0 +1,28 @@
+use async_trait::async_trait;
+use axum::{Extension, Router as AxumRouter};
+use fluent_templates::{ArcLoader, FluentLoader};
+use loco_rs::{
+    app::{AppContext, Initializer},
+    controller::views::{engines, ViewEngine},
+    Error, Result,
+};
+use tracing::info;
+
+const I18N_DIR: &str = "assets/i18n";
+const I18N_SHARED: &str = "assets/i18n/shared.ftl";
+#[allow(clippy::module_name_repetitions)]
+pub struct ViewEngineInitializer;
+
+#[async_trait]
+impl Initializer for ViewEngineInitializer {
+    fn name(&self) -> String {
+        "view-engine".to_string()
+    }
+
+    async fn after_routes(&self, router: AxumRouter, _ctx: &AppContext) -> Result<AxumRouter> {
+        // Просто создаем движок без FTL/локализации
+        let tera_engine = engines::TeraView::build()?;
+
+        Ok(router.layer(Extension(ViewEngine::from(tera_engine))))
+    }
+}
